31f72cfb19ad471fc650b87015d81dfac9271757
Sayeem M Shakil <97893755+sayeemmshakil@users.noreply.github.com>
2022-06-03 00:15:58 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ 'use strict'
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
